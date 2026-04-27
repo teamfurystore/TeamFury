401279bbd215dec0c9e01b9147db40cd8a516f19
ShrikeshB <shrikeshbandivadekarpro95@gmail.com>
2026-04-27 22:34:03 +0530

    supabase connection testing

---
+++ app/api/test/route.ts
@@ -0,0 +1,16 @@
+import { supabase } from "@/utils/supabaseClient";
+import { NextResponse } from "next/server";
+
+// GET API (fetch data only)
+export async function GET() {
+  try {
+    const { data, error } = await supabase.from("Test").select("*");
+
+    if (error) throw error;
+    console.log("DATA:", data);
+    console.log("ERROR:", error);
+    return NextResponse.json(data, { status: 200 });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
